2ab6ff821a0cb8ec9a020d16b51dabe4d0d5efc0
nashesizer <35979632+nashesizer@users.noreply.github.com>
2018-01-31 00:19:27 +0000

    Add files via upload

---
+++ bass_module_new_1/bass_module_new_1.ino
@@ -0,0 +1,126 @@
+///////////////////////////////////////
+/// LIBRARIES + GLOBALS
+///////////////////////////////////////
+
+#include <Wire.h> // I2C
+#include "Keyboard.h" // Teensy as an emulated keyboard
+#include <StopWatch.h>
+
+// single byte requested from joystick module
+byte bJs, bJsLast;
+byte jsUp = B1000;
+byte jsDown = B0100;
+byte jsLeft = B0010;
+byte jsRight = B0001;
+
+// for rolling polling of I2C bus
+int nextTime1  = 10; // do this every 10 milliseconds
+long int goTime1;
+
+// keyboard emulation
+#define OSX 0
+// change this to match your platform:
+int platform = OSX;
+
+/// StopWatch
+StopWatch jsHoldSW;
+
+
+///////////////////////////////////////
+/// SET UP
+///////////////////////////////////////
+
+void setup() {
+
+  Wire.begin(0); // join i2c bus (address optional for master)
+  Serial.begin(230400); // start serial for Serial Monitor output
+  Keyboard.begin();
+
+}
+
+
+///////////////////////////////////////
+/// MAIN LOOP
+///////////////////////////////////////
+
+void loop() {
+
+  // rolling polling of I2C slave modules
+  if (millis() >= goTime1) functionReadJoystick();
+
+}
+
+
+///////////////////////////////////////
+/// Nashesizer module functions
+///////////////////////////////////////
+void functionReadJoystick() {
+
+  Wire.requestFrom(1, 1); // request 1 byte from slave device #1
+  while (Wire.available()) { // slave may send less than requested
+    bJs = Wire.read(); // receive a byte as character
+  }
+  ////debugging
+  //Serial.print(bJs, BIN); // print the byte
+  //Serial.print(",");
+
+  if ((bJs == jsUp) && (bJsLast != bJs)) { // if a new joystick UP byte is received and the previous byte received was not UP
+    Keyboard.press(KEY_UP_ARROW); // send a key press
+    Keyboard.releaseAll();
+    // debugging
+    //Serial.print("bJs: ");
+    //Serial.print(bJs);
+    //Serial.print(", bJsLast: ");
+    //Serial.println(bJsLast);
+  } else if ((bJs == jsDown) && (bJsLast != bJs)) { // else if a new joystick DOWN byte is received and the previous byte received was not DOWN
+    Keyboard.press(KEY_DOWN_ARROW);
+    Keyboard.releaseAll();
+    // debugging
+    //Serial.print("bJs: ");
+    //Serial.print(bJs);
+    //Serial.print(", bJsLast: ");
+    //Serial.println(bJsLast);
+  } else if ((bJs == jsRight) && (bJsLast != bJs)) { // else if a new joystick RIGHT byte is received and the previous byte received was not RIGHT
+    Keyboard.press(KEY_RIGHT_ARROW);
+    Keyboard.releaseAll();
+    // debugging
+    //Serial.print("bJs: ");
+    //Serial.print(bbJs);
+    //Serial.print(", bJsLast: ");
+    //Serial.println(bJsLast);
+  } else if ((bJs == jsLeft) && (bJsLast != bJs)) { // else if a new joystick LEFT byte is received and the previous byte received was not LEFT
+    Keyboard.press(KEY_LEFT_ARROW);
+    Keyboard.releaseAll();
+    // debugging
+    //Serial.print("bJs: ");
+    //Serial.print(bbJs);
+    //Serial.print(", bJsLast: ");
+    //Serial.println(bJsLast);
+  } else {
+    // debugging
+    //Serial.print("bJs: ");
+    //Serial.print(bbJs);
+    //Serial.print(", bJsLast: ");
+    //Serial.println(bJsLast);
+  }
+
+  if (bJsLast == bJs) { // if the new byte is the same as the last byte - joystick is being held - start the stopwatch
+    jsHoldSW.start();
+  } else { // otherwise stop and reset the stopwatch
+    jsHoldSW.stop();
+    jsHoldSW.reset();
+  }
+  // debugging
+  //    Serial.print("jsHoldSW.elapsed(): ");
+  //    Serial.println(jsHoldSW.elapsed());
+
+  // use the stopwatch to reset the last byte to CENTRE after a set time so that the code above retriggers creating an auto repeat key press
+  if ((jsHoldSW.elapsed() > 250) && ((bJs == jsUp) || (bJs == jsDown) || (bJs == jsRight) || (bJs == jsLeft))) {
+    bJsLast = B00000000;
+  } else {
+    bJsLast = bJs;
+  }
+
+  goTime1 = millis() + nextTime1;
+
+}
---
+++ joystick_module_3/joystick_module_3.ino
@@ -0,0 +1,165 @@
+///////////////////////////////////////
+/// LIBRARIES + GLOBALS
+///////////////////////////////////////
+
+#include <Wire.h> // I2C
+#include <Adafruit_NeoPixel.h>
+#include <StopWatch.h>
+
+/// Neopixels
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN 4
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS 4
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+/// joystick
+#define LRPin A0
+#define UDPin A1
+int sensorValue[2] = {0, 0}; // variable array to store the values read from the joystick's analogue inputs
+byte joystick; // joystick position as least significant bits within a single byte - requested via I2C by Teensy 3.6
+/*
+   B1000 - UP
+   B0100 - DOWN
+   B0010 - LEFT
+   B0001 - RIGHT
+   B00000000 - CENTRE
+*/
+
+/// StopWatch - for LED colour change on joystick hold
+StopWatch jsHoldSW;
+
+
+///////////////////////////////////////
+/// SET UP
+///////////////////////////////////////
+
+void setup() {
+
+  // define A0 & A1 as inputs
+  pinMode(LRPin, INPUT);
+  pinMode(UDPin, INPUT);
+
+  Serial.begin(9600);
+  Wire.begin(1); // join i2c bus with address #1
+  Wire.onRequest(requestEvent); // register event - function below
+
+  pixels.begin(); // This initializes the NeoPixel library.
+  pixels.show(); // Initialize all pixels to 'off'
+
+  /// flash all LEDs on start up
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 32, 0)); // green
+    pixels.show(); // sends the updated pixel color to the hardware.
+  }
+  delay(500);
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // off
+    pixels.show();
+  }
+
+}
+
+
+///////////////////////////////////////
+/// MAIN LOOP
+///////////////////////////////////////
+
+void loop() {
+
+  // read the value from the joystick:
+  sensorValue[0] = analogRead(LRPin);
+  sensorValue[1] = analogRead(UDPin);
+
+  //// debugging - to read sensorValue[0] and sensorValue[1] in Serial Monitor
+  //  jsHoldSW.start();
+  //  if (jsHoldSW.elapsed() > 500) {
+  //    Serial.print("sensorValue[0]: ");
+  //    Serial.println(sensorValue[0]);
+  //    Serial.print("sensorValue[1]: ");
+  //    Serial.println(sensorValue[1]);
+  //    jsHoldSW.stop();
+  //    jsHoldSW.reset();
+  //  }
+
+  /*
+    centre - sensorValue[0] = 622; sensorValue[1] = 622;
+    up - sensorValue[0] = 594; sensorValue[1] = 1023;
+    down - sensorValue[0] = 608; sensorValue[1] = 0;
+    left - sensorValue[0] = 1023; sensorValue[1] = 593;
+    right - sensorValue[0] = 0; sensorValue[1] = 608;
+  */
+  /*
+    NeoPixels - 0 - down, 1 - right, 2 - up, 3 - left
+  */
+
+  if (sensorValue[1] > 970) { // UP
+    //Serial.println("UP");
+    joystick = B1000;
+    // use to StopWatch to turn LED from green to blue when held
+    jsHoldSW.start();
+    if (jsHoldSW.elapsed() < 250) {
+      pixels.setPixelColor(2, pixels.Color(0, 32, 0));
+      pixels.show();
+    } else {
+      pixels.setPixelColor(2, pixels.Color(0, 0, 32));
+      pixels.show();
+    }
+  } else if (sensorValue[1] < 50) { // DOWN
+    //Serial.println("DOWN");
+    joystick = B0100;
+    jsHoldSW.start();
+    if (jsHoldSW.elapsed() < 250) {
+      pixels.setPixelColor(0, pixels.Color(0, 32, 0));
+      pixels.show();
+    } else {
+      pixels.setPixelColor(0, pixels.Color(0, 0, 32));
+      pixels.show();
+    }
+    //digitalWrite(ledPin, HIGH);
+  } else if (sensorValue[0] > 970) { // LEFT
+    //Serial.println("LEFT");
+    joystick = B0010;
+    jsHoldSW.start();
+    if (jsHoldSW.elapsed() < 250) {
+      pixels.setPixelColor(1, pixels.Color(0, 32, 0));
+      pixels.show();
+    } else {
+      pixels.setPixelColor(1, pixels.Color(0, 0, 32));
+      pixels.show();
+    }
+  } else if (sensorValue[0] < 50) { // RIGHT
+    //Serial.println("RIGHT");
+    joystick = B0001;
+    jsHoldSW.start();
+    if (jsHoldSW.elapsed() < 250) {
+      pixels.setPixelColor(3, pixels.Color(0, 32, 0));
+      pixels.show();
+    } else {
+      pixels.setPixelColor(3, pixels.Color(0, 0, 32));
+      pixels.show();
+    }
+  } else { // CENTRE
+    joystick = B00000000;
+    jsHoldSW.stop();
+    jsHoldSW.reset();
+    for (int i = 0; i < NUMPIXELS; i++) {
+      pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+      pixels.show();
+    }
+  }
+}
+
+// function that executes whenever data is requested by I2C master
+// this function is registered as an event, see setup()
+void requestEvent() {
+  Wire.write(joystick); // respond with message of 1 byte as expected by master
+}
